ae7e26c7d645016114e971e2349ec67b0e293dbc
Dowon Kang <dev@dowonkang.name>
2021-05-03 17:53:41 +0900

    Add code samples

---
+++ samples/sample.js
@@ -0,0 +1,19 @@
+function draw() {
+  if (pressed) {
+    ctx.fillStyle = colorPicker.value;
+    ctx.beginPath();
+    ctx.arc(
+      curX,
+      curY - 85,
+      sizePicker.value,
+      degToRad(0),
+      degToRad(360),
+      false
+    );
+    ctx.fill();
+  }
+
+  requestAnimationFrame(draw);
+}
+
+draw();
